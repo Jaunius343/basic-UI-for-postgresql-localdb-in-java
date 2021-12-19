4347c99427e44a47b2cfdcbc733b1d4787ce542f
Jaunius343 <73691989+Jaunius343@users.noreply.github.com>
2021-12-19 16:14:03 +0200

    add transaction, add update

--- src/PostgreSQLJDBC.java
+++ src/PostgreSQLJDBC.java
@@ -15,7 +15,7 @@ public class PostgreSQLJDBC {
 
         nl();
         System.out.println("Table list:");
-        String[] tables = {"vartotojas", "saskaita", "kortele"};
+        String[] tables = {"vartotojas", "saskaita", "kortele", "bankas", "paslauga"};
         for(int i = 0; i < tables.length; ++i){
             System.out.println((i+1) + ". " + tables[i]);
         }
@@ -45,6 +45,11 @@ public class PostgreSQLJDBC {
             Statement statement = c.createStatement();
 
             String[] actions  = {"print whole table", "insert data", "update data", "delete data"};
+            if (selectedTable.equals("saskaita")){
+                actions  = new String[]{"print whole table", "insert data", "update data", "delete data", "transfer money", "Search by bank"};
+            }
+
+            String[] availableActions = actions;
             int selectedAction;
             boolean flag = false;
             while(!flag){
@@ -61,7 +66,7 @@ public class PostgreSQLJDBC {
                         sc.next(); // this is important!
                     }
                     number = sc.nextInt();
-                } while (number < 1 || number > 5);
+                } while (number < 1 || number > actions.length);
                 selectedAction = number;
 
                 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -81,14 +86,33 @@ public class PostgreSQLJDBC {
                         System.out.println("added");
                         break;
                     case 3:
-                        updateData(forMd, selectedTable);
-                        System.out.println("not implemented");
+                        statement.executeUpdate(updateData(forMd, selectedTable));
+                        System.out.println("updated");
                         break;
                     case 4:
                         sql = "DELETE FROM " + selectedTable + " WHERE id = ";
-                        int id = Integer.parseInt(br.readLine());
+                        System.out.println("Enter id to delete:");
+                        long id = Long.parseLong(br.readLine());
                         sql += id;
                         statement.executeUpdate(sql);
+                        System.out.println("deleted");
+                        break;
+                    case 5:
+                        doTranscation(statement, c);
+                        break;
+                    case 6:
+                        sql = "SELECT id, pavadinimas FROM bank.bankas";
+                        result = statement.executeQuery(sql);
+                        printTable(result);
+                        System.out.println("Select a bank for account display");
+
+                        int bankId = Integer.parseInt(br.readLine());
+                        sql = "SELECT saskaita.id, vartotojas, balansas, tipas, pavadinimas " +
+                                "FROM bank.saskaita JOIN bank.bankas " +
+                                "ON bankas.id = saskaita.bankas " +
+                                "WHERE bankas.id = " + bankId;
+                        result = statement.executeQuery(sql);
+                        printTable(result);
                         break;
                     default:
                         System.out.println("should never happen");
@@ -101,29 +125,104 @@ public class PostgreSQLJDBC {
         } catch (Exception e) {
             e.printStackTrace();
             System.err.println(e.getClass().getName()+": "+e.getMessage());
-            System.exit(0);
+            //System.out.println(e.getClass().getName()+": "+e.getMessage());
+            //System.exit(0);
         }
         //System.out.println("Opened database successfully");
     }
 
-    static public String updateData(ResultSet rs, String table)
+    static public void doTranscation(Statement statement, Connection c)
     {
         String sql;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try{
+            c.setAutoCommit(false);
+
+            System.out.println("Transfer money from: ");
+            int num = Integer.parseInt(br.readLine());
+            System.out.println("Transfer money to: ");
+            int num2 = Integer.parseInt(br.readLine());
+            System.out.println("Transfer amount: ");
+
+            int sum = Integer.parseInt(br.readLine());
+            sql = "UPDATE saskaita SET balansas = balansas + " + sum + " WHERE id = " + num2;
+            statement.executeUpdate(sql);
+
+//            System.out.println(sql);
+
+            sql = "UPDATE saskaita SET balansas = balansas - " + sum + " WHERE id = " + num;
+            statement.executeUpdate(sql);
+//            System.out.println(sql);
+
+            c.commit();
+            System.out.println("transaction successful");
+        } catch (Exception e){
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            try{
+                System.out.println("Transaction is being rolled back");
+                c.rollback();
+            } catch (Exception ex){
+                ex.printStackTrace();
+                System.err.println(ex.getClass().getName()+": "+ex.getMessage());
+            }
+        } finally {
+            try{
+                c.setAutoCommit(true);
+            } catch (Exception ex){
+                ex.printStackTrace();
+                System.err.println(ex.getClass().getName()+": "+ex.getMessage());
+            }
+        }
+    }
+
+    static public String updateData(ResultSet rs, String table)
+    {
+        String sql = "UPDATE " + table + " SET ";
+        try{
+            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
             System.out.println("Data update selected");
             List<String> fields = new ArrayList<>();
 
             ResultSetMetaData rsmd = rs.getMetaData();
             int collumnCount = rsmd.getColumnCount();
 
-            for(int i = 2 ; i <= collumnCount; i++){
-                fields.add(rsmd.getColumnName(i));
+            for(int i = 2 ; i <= collumnCount; ++i){
+                System.out.println("Do you want to update "+ rsmd.getColumnName(i) + ": y/n");
+                String s=br.readLine();
+                if(s.startsWith("y")){
+                    System.out.println("New value for " + rsmd.getColumnName(i));
+                    if (rsmd.getColumnClassName(i).equals("java.lang.String")){
+                        String temp = br.readLine();
+                        sql += rsmd.getColumnName(i) + " = '" + temp + "', ";
+                    }
+                    if(rsmd.getColumnClassName(i).equals("java.sql.Date")){
+                        String date = br.readLine();
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                        Date temp = dateFormat.parse(date);
+                        sql += rsmd.getColumnName(i) + " = '"  + dateFormat.format(temp) + "', ";
+                    }
+                }
             }
-            int temp = fields.size();
-            int[] selectedFields = new int[temp];
+            sql = sql.substring(0, sql.length() - 2);
+            System.out.println("User ID to update: ");
+//            int id = Integer.parseInt(br.readLine());
+            long id = Long.parseLong(br.readLine());
+            sql += " WHERE id = " + id;
 
-            System.out.println(fields);
-            System.out.println(fields.size());
+            return sql;
+//            for(int i = 0 ; i <= fields.size(); ++i) {
+//                System.out.println("New value for " + fields.get(i));
+//                String s = br.readLine();
+//            }
+
+
+//            int temp = fields.size();
+//            int[] selectedFields = new int[temp];
+
+//            System.out.println(fields);
+//            System.out.println(fields.size());
 
             //select fields to update
             //y/n if y, selectedfiels[i] = 1;
@@ -158,7 +257,7 @@ public class PostgreSQLJDBC {
 
             for(int i = 2 ; i <= collumnCount; i++){
 //                sql += rsmd.getColumnName(i) + ", ";
-                System.out.print(rsmd.getColumnClassName(i) + " ");
+//                System.out.print(rsmd.getColumnClassName(i) + " ");
                 System.out.println(rsmd.getColumnName(i) + ":");
 
                 if (rsmd.getColumnClassName(i).equals("java.lang.String")){
@@ -171,11 +270,19 @@ public class PostgreSQLJDBC {
                     Date temp = dateFormat.parse(date);
                     sql += "'" + dateFormat.format(temp) + "', ";
                 }
+                if(rsmd.getColumnClassName(i).equals("java.lang.Integer")){
+                    int num = Integer.parseInt(br.readLine());
+                    sql += num + ", ";
+                }
+                if(rsmd.getColumnClassName(i).equals("java.lang.Long")){
+                    long num = Long.parseLong(br.readLine());
+                    sql += num + ", ";
+                }
             }
             sql = sql.substring(0, sql.length() - 2);
             sql = sql + ")";
 
-            //System.out.println(sql);
+//            System.out.println(sql);
 
             return sql;
         } catch(Exception e){
